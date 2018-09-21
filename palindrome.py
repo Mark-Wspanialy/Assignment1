b9366858cabf28ba96eca768e806c518c376ee72
Mark Wspanialy <markwasp@gmail.com>
2018-09-21 11:56:11 -0400

    palindrome fix

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,15 @@
 string_to_test = input("What word or phrase would you like to test as a palindrome?")
-string_to_test.replace(" ","")
-reversed_string = string_to_test[::-1]
-if reversed_string == string_to_test:
+def palindrome_test(check_string):
+    check_string.replace(" ","")
+    reverse_to_check = check_string[::-1]
+    if reverse_to_check == check_string:
+        check_string = "palindrome"
+        return check_string
+    else:
+        check_string = "incorrect"
+        return check_string
+check_string = palindrome_test(string_to_test)
+if check_string == "palindrome":
     print("This is a palindrome!")
 else:
     print("This is not a palindrome.")
